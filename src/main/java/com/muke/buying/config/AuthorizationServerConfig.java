75da32aecb32da8e90d9e7f4c14bf5f5870f693a
ylj758 <758766951@qq.com>
2021-05-24 13:52:37 +0800

    refresh_token

--- src/main/java/com/muke/buying/config/AuthorizationServerConfig.java
+++ src/main/java/com/muke/buying/config/AuthorizationServerConfig.java
@@ -1,11 +1,13 @@
 package com.muke.buying.config;
 
+import com.muke.buying.service.UserDetailsServiceImpl;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 /**
  * 授权服务器
@@ -16,6 +18,8 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+
+    private final UserDetailsServiceImpl userDetailsService;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -29,7 +33,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 // 授权范围
                 .scopes("all")
                 // 授权类型：授权码模式
-                .authorizedGrantTypes("authorization_code")
-                .accessTokenValiditySeconds(60);
+                .authorizedGrantTypes("authorization_code","refresh_token")
+                .accessTokenValiditySeconds(60)
+                .refreshTokenValiditySeconds(180);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.userDetailsService(userDetailsService);
     }
 }
