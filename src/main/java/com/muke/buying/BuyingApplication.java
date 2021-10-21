ca99889ed19defab5940700f4e0920b0b447a851
lvjun.yu <lvjun.yu@thoughtworks.com>
2021-10-21 10:59:45 +0800

    feat: 本地tomcat启动

--- src/main/java/com/muke/buying/BuyingApplication.java
+++ src/main/java/com/muke/buying/BuyingApplication.java
@@ -2,12 +2,17 @@ package com.muke.buying;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BuyingApplication {
+public class BuyingApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BuyingApplication.class, args);
+//    public static void main(String[] args) {
+//        SpringApplication.run(BuyingApplication.class, args);
+//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BuyingApplication.class);
     }
-
 }
